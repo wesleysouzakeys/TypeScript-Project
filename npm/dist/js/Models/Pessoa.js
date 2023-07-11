3930ebe85cf73ea3c8f31bee7cde34fc9357a0ce
Wesley <wmixgamer@gmail.com>
2023-07-11 08:38:05 -0300

    ignoring dist/js

--- npm/dist/js/Models/Pessoa.js
+++
@@ -1,8 +0,0 @@
-export class Pessoa {
-    constructor(name, role, image, team) {
-        this.name = name;
-        this.role = role;
-        this.image = image;
-        this.team = team;
-    }
-}
